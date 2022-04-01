aadb28ed03d2b3eef4e6a05b6578aea2878ea486
Brann <brann@woowahan.com>
2022-04-01 23:53:27 +0900

    MainViewModel 구현 closes #4

---
+++ GithubRepositorySearcher/core/di/InjectedStateObject.swift
@@ -0,0 +1,8 @@
+//
+//  InjectedStateObject.swift
+//  GithubRepositorySearcher
+//
+//  Created by brann on 2022/04/01.
+//
+
+import Foundation
---
+++ GithubRepositorySearcher/presentation/scene/main/MainViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  MainViewModel.swift
+//  GithubRepositorySearcher
+//
+//  Created by brann on 2022/04/01.
+//
+
+import Foundation
+import Combine
+
+class MainViewModel: ObservableObject, RoutableViewModel {
+
+    private let getGitHubRepositoriesUsecase: GetGitHubRepositoriesUsecase
+    private var disposeBag = Set<AnyCancellable>()
+    @Published var repositories = [Repository]()
+    @Published var isLoading = false
+    let router = PassthroughRelay<Routing>()
+
+    init(getGitHubRepositoriesUsecase: GetGitHubRepositoriesUsecase) {
+        self.getGitHubRepositoriesUsecase = getGitHubRepositoriesUsecase
+    }
+
+    func submit(searchQuery: String) {
+        repositories = []
+        getGitHubRepositoriesUsecase.execute(query: searchQuery)
+            .handleEvents(receiveSubscription: { [weak self] _ in
+                self?.isLoading = true
+            }, receiveCompletion: { [weak self] _ in
+                self?.isLoading = false
+            }, receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
+            .sink { completion in
+                guard case .failure(let error) = completion else {
+                    return
+                }
+                print(error)
+            } receiveValue: { repositories in
+                self.repositories = repositories
+            }
+            .store(in: &disposeBag)
+    }
+
+    func didTap(item: Repository) {
+        router.send(.toWebScene(url: item.linkURL))
+    }
+}
+
+extension MainViewModel {
+
+    enum Routing {
+
+        case toWebScene(url: URL)
+    }
+}

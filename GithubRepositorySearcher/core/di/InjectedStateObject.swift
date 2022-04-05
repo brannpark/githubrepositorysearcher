439d2c4a36f2dfe6cbfc1abad4b019cf265730f2
Brann <brann@woowahan.com>
2022-04-05 13:21:33 +0900

    InjectedStateObject 초기화 시 마다 DI Injection 일어나는 문제 수정

--- GithubRepositorySearcher/core/di/InjectedStateObject.swift
+++ GithubRepositorySearcher/core/di/InjectedStateObject.swift
@@ -5,30 +5,82 @@
 //  Created by brann on 2022/04/01.
 //
 
-import SwiftUI
 import Resolver
+import SwiftUI
+import Combine
 
-@propertyWrapper struct InjectedStateObject<Service>: DynamicProperty where Service: ObservableObject {
+@propertyWrapper
+public struct InjectedStateObject<ObjectType>: DynamicProperty where ObjectType: ObservableObject {
 
-    @StateObject private var service: Service
+    var initializer: ((ObjectType) -> Void)?
+    @State private var box = RestorableBox()
 
-    init(_ initializer: ((Service) -> Void)? = nil) {
-        let service = Resolver.main.resolve(Service.self)
-        initializer?(service)
-        _service = StateObject(wrappedValue: service)
+    private final class RestorableBox {
+
+        var object: ObjectType?
     }
 
-    init(name: Resolver.Name? = nil, container: Resolver? = nil, initializer: ((Service) -> Void)? = nil) {
-        let service = container?.resolve(Service.self, name: name) ?? Resolver.main.resolve(Service.self, name: name)
-        initializer?(service)
-        _service = StateObject(wrappedValue: service)
+    private final class MessageForwarder: ObservableObject {
+
+        let objectWillChange = ObservableObjectPublisher()
+        var subscription: AnyCancellable?
+        var object: ObjectType? {
+            didSet {
+                // Subscribe to the object only if needed.
+                if let object = object, object !== oldValue {
+                    subscription = object.objectWillChange.sink { [weak self] _ in
+                        self?.objectWillChange.send()
+                    }
+                }
+            }
+        }
     }
 
-    var wrappedValue: Service {
-        service
+    @ObservedObject private var messageForwarder = MessageForwarder()
+
+    public var wrappedValue: ObjectType {
+        if let object = messageForwarder.object {
+            return object
+        } else {
+            fatalError(
+          """
+          BUG: This should never happen as `update` is guaranteed to mutate \
+          before you're allowed to access `wrappedValue` property.
+          """
+            )
+        }
     }
 
-    var projectedValue: ObservedObject<Service>.Wrapper {
-        return $service
+    public var projectedValue: ObservedObject<ObjectType>.Wrapper {
+        if let object = messageForwarder.object {
+            let wrapper = ObservedObject(wrappedValue: object)
+            return wrapper.projectedValue
+        } else {
+            fatalError(
+        """
+        BUG: This should never happen as `update` is guaranteed to mutate \
+        before you're allowed to access `projectedValue` property.
+        """
+            )
+        }
+    }
+
+    public init() {}
+
+    public init(_ initializer: @escaping (ObjectType) -> Void) {
+        self.initializer = initializer
+    }
+
+    public func update() {
+        // This mutation should only happen once for the lifetime of the view.
+        if box.object == nil {
+            let obj = Resolver.main.resolve(ObjectType.self)
+            initializer?(obj)
+            box.object = obj
+        }
+        // Re-inject the object into the message forwarder.
+        if let object = box.object {
+            messageForwarder.object = object
+        }
     }
 }
